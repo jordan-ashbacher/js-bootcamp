efa70061cb7cf7a34a069fd01383e8ee39a1a340
jordan-ashbacher <jordan.ashbacher@gmail.com>
2020-08-24 15:21:35 -0500

    creates expense manager program

---
+++ objects/expense-manager.js
@@ -0,0 +1,29 @@
+let myAccount = {
+	name: 'Jordan Ashbacher',
+	expenses: 0,
+	income: 0
+};
+
+let addExpense = function(account, amount) {
+	account.expenses += amount;
+};
+
+let addIncome = function(account, amount) {
+	account.income += amount;
+};
+
+let resetAccount = function(account) {
+	account.expenses = 0;
+	account.income = 0;
+};
+
+let getAccountSummary = function(account) {
+	return `Account of ${account.name} has a balance of ${account.income -
+		account.expenses}. ${account.income} in income. ${account.expenses} in expenses.`;
+};
+
+addIncome(myAccount, 1600);
+addExpense(myAccount, 1000);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
