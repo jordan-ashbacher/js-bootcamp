e4a5246026d985905063a46bd295ca0f304ce38d
jordan-ashbacher <jordan.ashbacher@gmail.com>
2020-08-25 11:01:56 -0500

    creates arrays directory and starting file for notes and task manager applications

---
+++ arrays/todo.js
@@ -0,0 +1,13 @@
+const tasks = [
+	'Study Javascript',
+	'Practice yoga',
+	'Improve jumpshot',
+	'Go on dates',
+	'Run a half-marathon',
+	'Build a website',
+	'Take over the world'
+];
+
+console.log(`You have ${tasks.length} todos on your list.`);
+console.log(`To-do: ${tasks[0]}`);
+console.log(`To-do: ${tasks[tasks.length - 2]}`);
