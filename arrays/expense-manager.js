896d7c08194a48cc0b3ff0c9da20cc98988b9da1
jordan-ashbacher <jordan.ashbacher@gmail.com>
2020-08-28 16:18:40 -0500

    creates addIncome function and updates getAccountSummary function to include income and balance

--- arrays/expense-manager.js
+++ arrays/expense-manager.js
@@ -1,23 +1,39 @@
 const account = {
 	name: 'Jordan Ashbacher',
 	expenses: [],
+	income: [],
 	addExpense: function(newExpenseDescription, newExpenseAmount) {
-		return this.expenses.push({
+		this.expenses.push({
 			description: newExpenseDescription,
 			amount: newExpenseAmount
 		});
 	},
+	addIncome: function(newIncomeDescription, newIncomeAmount) {
+		this.income.push({
+			description: newIncomeDescription,
+			amount: newIncomeAmount
+		});
+	},
 	getAccountSummary: function() {
-		let total = 0;
-		for (i = 0; i < this.expenses.length; i++) {
-			total += this.expenses[i].amount;
-		}
-		return `${this.name} has $${total} in expenses.`;
+		let totalExpenses = 0;
+		let totalIncome = 0;
+		let initialBalance = 0;
+		this.expenses.forEach(function(expense) {
+			totalExpenses += expense.amount;
+		});
+		this.income.forEach(function(income) {
+			totalIncome += income.amount;
+		});
+		initialBalance = totalIncome - totalExpenses;
+		let accountBalance = initialBalance.toFixed(2);
+		return `${this
+			.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses.`;
 	}
 };
 
 account.addExpense('Rent', 300);
 account.addExpense('Beer', 10.95);
+account.addIncome('Unemployment', 407);
 console.log(account.getAccountSummary());
 
 // Original getAccountSummary solution using for loop:
