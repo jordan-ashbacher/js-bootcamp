de703198329a2763420b4d8fefcc37dbc5ae0bb1
jordan-ashbacher <jordan.ashbacher@gmail.com>
2020-10-02 17:25:34 -0500

    creates hangman app and object oriented programming files

---
+++ oop/person.js
@@ -0,0 +1,27 @@
+const Person = function(firstName, lastName, age, likes = []) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.age = age;
+	this.likes = likes;
+};
+
+Person.prototype.getBio = function() {
+	let bio = `${this.firstName} is ${this.age}.`;
+	this.likes.forEach((like) => {
+		bio += ` ${this.firstName} likes ${like}.`;
+	});
+	return bio;
+};
+
+Person.prototype.setName = function(fullName) {
+	const names = fullName.split(' ');
+	this.firstName = names[0];
+	this.lastName = names[1];
+};
+
+const me = new Person('Jordan', 'Ashbacher', 29, [ 'basketball', 'metal' ]);
+me.setName('Elon Musk');
+console.log(me.getBio());
+
+const person2 = new Person('Ali', 'Ashbacher', 25);
+console.log(person2.getBio());
